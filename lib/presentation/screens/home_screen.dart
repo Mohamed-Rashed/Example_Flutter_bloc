209e96e5c6f1fe86a1e8ead2e39a0f4e0b02e68a
Mohamed-Rashed <mohamedrashed88@gmail.com>
2021-09-21 13:56:38 +0200

    Search git

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -18,6 +18,87 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _isSearching = false;
   final _searchTextContrllor = TextEditingController();
 
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _searchTextContrllor,
+      cursorColor: MyColor.kgray,
+      decoration: InputDecoration(
+        hintText: "Find Character",
+        border: InputBorder.none,
+        hintStyle: TextStyle(
+          color: MyColor.kgray,
+          fontSize: 18,
+        ),
+      ),
+      style: TextStyle(fontSize: 18, color: MyColor.kgray),
+      onChanged: (searchedCharacter) {
+        addSearchedForItemsToSearchedList(searchedCharacter);
+      },
+    );
+  }
+
+  void addSearchedForItemsToSearchedList(String searchedCharacter) {
+    searchedForCharacters = allCharacters!
+        .where((Character) =>
+            Character.name.toLowerCase().startsWith(searchedCharacter))
+        .toList();
+    setState(() {});
+  }
+
+  List<Widget> _buildAppBarActions() {
+    if (_isSearching) {
+      return [
+        IconButton(
+          onPressed: () {
+            _clearSearch();
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.clear, color: MyColor.kgray),
+        ),
+      ];
+    } else {
+      return [
+        IconButton(
+          onPressed: _startSearch,
+          icon: Icon(
+            Icons.search,
+            color: MyColor.kgray,
+          ),
+        ),
+      ];
+    }
+  }
+
+  void _clearSearch() {
+    setState(() {
+      _searchTextContrllor.clear();
+    });
+  }
+
+  void _startSearch() {
+    ModalRoute.of(context)!
+        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void _stopSearching() {
+    _clearSearch();
+
+    setState(() {
+      _isSearching = false;
+    });
+  }
+
+  Widget _buildAppBarTitle() {
+    return Text(
+      'Characters',
+      style: TextStyle(color: MyColor.kwhite),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -36,6 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     );
   }
+
   Widget buildLoadedListWidgets() {
     return SingleChildScrollView(
       child: Container(
@@ -48,25 +130,42 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
   Widget buildCharactersList() {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2 / 3,
-        crossAxisSpacing: 1,
-        mainAxisSpacing: 1,
-      ),
-      shrinkWrap: true,
-      physics: const ClampingScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: allCharacters!.length,
-      itemBuilder: (ctx, index) {
-        return CharacterItem(
-          character:  allCharacters![index],
-        );
-      },
-    );
+    return _searchTextContrllor.text.isNotEmpty &&
+            searchedForCharacters!.length == 0
+        ? Column(
+            children: [
+              Image.asset('assets/images/sorry.gif'),
+              Text(
+                "sorry no results found :(",
+                style: TextStyle(color: MyColor.kwhite, fontSize: 18),
+              )
+            ],
+          )
+        : GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 2 / 3,
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 1,
+            ),
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: _searchTextContrllor.text.isEmpty
+                ? allCharacters!.length
+                : searchedForCharacters!.length,
+            itemBuilder: (ctx, index) {
+              return CharacterItem(
+                character: _searchTextContrllor.text.isEmpty
+                    ? allCharacters![index]
+                    : searchedForCharacters![index],
+              );
+            },
+          );
   }
+
   Widget showLoadingIndicator() {
     return Center(
       child: CircularProgressIndicator(
@@ -78,11 +177,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColor.kgray,
       appBar: AppBar(
         backgroundColor: MyColor.kyellow,
-        title: Text("Movies",style: TextStyle(
-          color: MyColor.kwhite,
-        ),),
+        leading: _isSearching
+            ? BackButton(
+                color: MyColor.kgray,
+              )
+            : Container(),
+        title: _isSearching ? _buildSearchField() : _buildAppBarTitle(),
+        actions: _buildAppBarActions(),
       ),
       body: buildBlocWidget(),
     );
